24446b3c36f5bd49c7ac43f5d3a50724a950c1b0
Elvin Gao <elvingaocn@gmail.com>
2019-03-19 23:13:00 +1100

    Column changes and Provision table created
    Changed the nvarchar length of the category column in Task and Category tables to 50
    Added Duration column in Activity table
    Changed the wrong column name CreatedOn in Activity to EndTime
    Added Provision table

--- timeManagementDBCreation.sql
+++ timeManagementDBCreation.sql
@@ -1,11 +1,15 @@
-use [Time Management]
+CREATE DATABASE [Time Management]
+GO
+
+USE [Time Management]
+GO
 
 CREATE TABLE Task
 (
 	AutoIdx int IDENTITY(1,1) NOT NULL,
 	Name nvarchar(50) NOT NULL,
 	Description nvarchar(100),
-	Category nvarchar(25),
+	Category nvarchar(50),
 	Active bit NOT NULL
 )
 GO
@@ -28,7 +32,7 @@ GO
 CREATE TABLE Category
 (
 	AutoIdx int IDENTITY(1,1) NOT NULL,
-	Name nvarchar(25) NOT NULL
+	Name nvarchar(50) NOT NULL
 )
 GO
 
@@ -37,5 +41,20 @@ CREATE TABLE Activity
 	AutoIdx int IDENTITY(1,1) NOT NULL,
 	Task_ID int NOT NULL,
 	StartTime datetime NOT NULL,
-	CreatedOn datetime NOT NULL
+	EndTime datetime NOT NULL,
+	Duration time NOT NULL
 )
+GO
+
+CREATE TABLE Provision
+(
+	Autoidx int IDENTITY(1,1) NOT NULL,
+	Name nvarchar(50) NOT NULL,
+	Description nvarchar(100),
+	StartTime datetime NOT NULL,
+	EndTime datetime NOT NULL,
+	Duration time NOT NULL,
+	Note nvarchar(250),
+	Category nvarchar(50)
+)
+GO
