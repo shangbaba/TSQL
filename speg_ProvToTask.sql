3d9e23d9ea62fa7d1a631dc2f21d0d8381234215
Elvin Gao <elvingaocn@gmail.com>
2019-03-24 22:46:17 +1100

    Task table population

---
+++ speg_ProvToTask.sql
@@ -0,0 +1,19 @@
+ALTER PROCEDURE speg_ProvToTask
+AS
+
+WITH dedupedTask AS (
+SELECT DISTINCT p.Name, description, c.CategoryID AS Cat_ID
+FROM Provision p
+INNER JOIN Category c ON p.Category = c.Name)
+
+MERGE Task AS TARGET
+USING dedupedTask AS SOURCE ON TARGET.Name = SOURCE.Name 
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT (Name, Description, Category_ID)
+	VALUES (SOURCE.Name, SOURCE.Description, SOURCE.Cat_ID)
+OUTPUT $action,
+	INSERTED.Name as newTask,
+	INSERTED.Description as newDescription,
+	INSERTED.Category_ID as newCategory;
+
+--EXEC speg_ProvToTask
