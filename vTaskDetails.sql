9c7c5a244cfc4c5795467b67ad80e658266238ce
Elvin Gao <elvingaocn@gmail.com>
2019-03-26 22:54:50 +1100

    Sumed the durations up

--- vTaskDetails.sql
+++ vTaskDetails.sql
@@ -6,14 +6,16 @@ SELECT
 	Description, 
 	c.Name AS Category, 
 	b.Monday + b.Tuesday + b.Wednesday + b.Thursday + b.Friday + b.Saturday + b.Sunday AS WeeklyBudget,
-	SUM(a.Duration) AS Actioned -- To be corrected. Time cannot be summed this way.
+	CAST(SUM(DATEDIFF(SECOND, '0:00:00', a.Duration))/60.00 AS decimal(19,2)) AS MinsActioned
 FROM TASK t
 LEFT JOIN Category c ON t.Category_ID = c.CategoryID
 LEFT JOIN Budget b ON t.TaskID = b.Task_ID
 LEFT JOIN Activity a on t.TaskID = a.Task_ID
 WHERE a.StartTime > GETDATE() - DATEPART(dw,getdate()) + 1 -- Start time greater than Monday this week
+GROUP BY TaskID, t.Name, Description, c.Name, (b.Monday + b.Tuesday + b.Wednesday + b.Thursday + b.Friday + b.Saturday + b.Sunday)
+
+--SET DATEFIRST 1; -- 1 for Monday as the first day of the week or 7 for Sunday as the first day of the week
+--SELECT DATEPART(dw,getdate()) as 'nth day of the week'
 
 
-SET DATEFIRST 1; -- 1 for Monday as the first day of the week or 7 for Sunday as the first day of the week
-SELECT DATEPART(dw,getdate()) as 'nth day of the week'
 
